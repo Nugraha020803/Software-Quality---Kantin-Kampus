71503664f65bb6ad4f7c8cf06c160cd3c89fe79e
Nugraha020803 <aryanugraha0203@gmail.com>
2025-06-08 18:35:59 +0700

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+<?php
+// footer.php
+?>
+    <footer>
+        <p>&copy; <?php echo date("Y"); ?> Kantin Kampus | Semua Hak Dilindungi</p>
+    </footer>
+    <script src="../assets/js/scripts.js" defer></script> <!-- Gunakan defer agar script dieksekusi setelah HTML dimuat -->
+
+
+</body>
+</html>
